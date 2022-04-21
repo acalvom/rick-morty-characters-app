c278e13d520bffc41f1a798e23fadf7589c76c34
acalvom <andreac.3108@gmail.com>
2022-04-21 16:10:57 +0200

    Fetch data & Update styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,56 @@
+import { Box, Container, Grid, Typography } from "@mui/material";
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import { useQuery } from "react-query";
+import CharacterCard from "../components/CharacterCard";
 
 const Home: NextPage = () => {
+  const getCharacters: any = async () => {
+    const res = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await res.json();
+    console.log(data);
+    return data;
+  };
+
+  const { data, status, error } = useQuery(["characters"], getCharacters);
+
+  console.log("query", data, status, error);
+
   return (
-    <div className={styles.container}>
+    <Container>
       <Head>
-        <title>Home</title>
+        <title>Rick and Morty</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Rick and Morty App
-    </div>
+      <Typography variant="h2" align="center" gutterBottom component="div">
+        Characters
+      </Typography>
+
+      <Box alignContent="center">
+        <Grid className="card-list" container spacing={1}>
+          <Grid item>
+            <CharacterCard />
+          </Grid>
+          <Grid item>
+            <CharacterCard />
+          </Grid>
+          <Grid item>
+            <CharacterCard />
+          </Grid>
+          <Grid item>
+            <CharacterCard />
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
   );
 };
 
 export default Home;
+
+// export const getStaticProps: GetStaticProps = async ({}) => {
+//   const res = await fetch("https://rickandmortyapi.com/api/character");
+//   const data = await res.json();
+//   console.log(data);
+//   return data;
+// };
