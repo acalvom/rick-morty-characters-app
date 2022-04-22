fa9f2d82b1c485279e635b7942990c02a61f2ccc
acalvom <andreac.3108@gmail.com>
2022-04-22 14:15:56 +0200

    Add pagination (in progress)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,28 @@
 import { Box, Container, Grid } from "@mui/material";
-import type { GetStaticProps, NextPage } from "next";
+import type { NextPage } from "next";
 import Head from "next/head";
-import { dehydrate, QueryClient, useQuery } from "react-query";
+import { useState } from "react";
+import { useQuery } from "react-query";
 import CharacterCard from "../components/CharacterCard";
 import { Characters } from "../interfaces/ICharacter";
 
-const getCharacters = async () => {
-  const res = await fetch("https://rickandmortyapi.com/api/character");
+const getCharacters = async (page: string) => {
+  const res = await fetch(
+    `https://rickandmortyapi.com/api/character/?page=${page}`
+  );
   const characters: Characters = await res.json();
   return characters;
 };
 
 const Home: NextPage = () => {
-  const { data } = useQuery("characters", getCharacters);
+  const [page, setPage] = useState("3");
+  const { data, isPreviousData } = useQuery(
+    ["characters", page],
+    () => getCharacters(page),
+    { keepPreviousData: true }
+  );
+
+  console.log(data);
 
   return (
     <Container className="main-container">
@@ -37,13 +47,14 @@ const Home: NextPage = () => {
 
 export default Home;
 
-export const getStaticProps: GetStaticProps = async ({}) => {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery("characters", getCharacters);
+// export const getStaticProps: GetStaticProps = async (context) => {
+//   const page = context.params?.page as string;
+//   const queryClient = new QueryClient();
+//   await queryClient.prefetchQuery(["characters", page], getCharacters(page));
 
-  return {
-    props: {
-      dehydratedState: dehydrate(queryClient),
-    },
-  };
-};
+//   return {
+//     props: {
+//       dehydratedState: dehydrate(queryClient),
+//     },
+//   };
+// };
